a3bc9fa21fc88bbc886a3da483c81f7880ce07e7
iKH2 <kmazhr11@gmail.com>
2022-09-06 02:42:55 +0300

    add bootstrap

--- index.php
+++ index.php
@@ -1,12 +1,53 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ar" dir="rtl">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Maktaba</title>
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhaijaan+2:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+    <!-- Bootstrap v5.2.0 -->
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <!-- Main CSS file -->
+    <link rel="stylesheet" href="css/one.css">
+    
 </head>
 <body>
-    <p>hello </p>
+    <!-- navbar -->
+    <div class="header">
+    <div class="overlay"></div>
+    <div class="banner-image">
+        <div class="container">
+            <h1>مكتبة أيمن</h1>
+        </div>
+        
+    </div>
+    </div>
+
+<!---------------------->
+<div class="name-section">
+    <div class="container">
+
+    </div>
+</div>
+
+<div class="name-section">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+            </div>
+        </div>
+    </div>
+</div>
+<!-------------------------------->
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+    <script src="js/scripts.js">
+
 </body>
 </html>
